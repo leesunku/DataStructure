301ba55f1b2d5284da3345dbd7b1f5d7de0f7a24
k <tjsrn0524@naver.com>
2016-12-15 20:53:11 +0900

    20161215

---
+++ DataStructure/selection_sort/selection_sort.js
@@ -0,0 +1,13 @@
+let ex = [30,24,6,23,15,67,85,3,2,67,0];
+
+for (let i = 0; i < ex.length; i++) {
+	for (let j = i+1; j < ex.length; j++) {
+		if ( ex[i] > ex[j]){
+			let tmp = ex[i]
+			ex[i] = ex[j];
+			ex[j] = tmp;
+			//console.log(ex.toString());			
+		}
+	}
+}
+console.log(ex.toString());
