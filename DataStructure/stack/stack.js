2ecc0bbd3ee7e08c893ca34cb315527fae864c81
k <tjsrn0524@naver.com>
2016-12-15 20:26:57 +0900

    20161215

---
+++ DataStructure/stack/stack.js
@@ -0,0 +1,26 @@
+/**
+stack : 후입선출 자료구조
+ */
+
+let STACK = new Array();
+let idx = 0
+
+
+const push = (data)=>{
+	STACK[idx++] = data;
+};
+const pop = ()=> {
+	if (idx === 0)
+		return 'empty';
+	return STACK[--idx];
+}
+const print = (data)=> {
+	console.log(data);
+}
+push('a');
+push('b');
+print(pop());
+push('c');
+print(pop());
+print(pop());
+print(pop());
